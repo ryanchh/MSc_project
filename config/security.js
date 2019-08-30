3f28fe90e5c7f3c35fe69a800c5f242e69e0b2c0
ryanchh <54244976+ryanchh@users.noreply.github.com>
2019-08-30 23:49:40 +0100

    disable csrf

--- config/security.js
+++ config/security.js
@@ -49,7 +49,7 @@ module.exports.security = {
   *                                                                           *
   ****************************************************************************/
 
-  // csrf: false
-  csrf: true
+  csrf: false
+  // csrf: true
 
 };
